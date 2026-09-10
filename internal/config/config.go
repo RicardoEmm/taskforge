e7fb74ed354d328233e44b5f115b27fa4817481d
Ricardo Emmanuel <remm@Ricardos-MacBook-Pro.local>
2026-09-10 11:58:03 -0600

    feat(config): add new variables to Config struct related with jwt

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,31 +2,38 @@ package config
 
 import (
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	AppPort    string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBSSLMode  string
+	AppPort         string
+	DBHost          string
+	DBPort          string
+	DBUser          string
+	DBPassword      string
+	DBName          string
+	DBSSLMode       string
+	JWTSecret       string
+	AccessTokenTTL  time.Duration
+	RefreshTokenTTL time.Duration
 }
 
 func Load() Config {
 	_ = godotenv.Load()
 
 	return Config{
-		AppPort:    getEnv("APP_PORT", "8080"),
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("POSTGRES_USER", "postgres"),
-		DBPassword: getEnv("POSTGRES_PASSWORD", "postgres"),
-		DBName:     getEnv("POSTGRES_DB", "taskforge_db"),
-		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+		AppPort:         getEnv("APP_PORT", "8080"),
+		DBHost:          getEnv("DB_HOST", "localhost"),
+		DBPort:          getEnv("DB_PORT", "5432"),
+		DBUser:          getEnv("POSTGRES_USER", "postgres"),
+		DBPassword:      getEnv("POSTGRES_PASSWORD", "postgres"),
+		DBName:          getEnv("POSTGRES_DB", "taskforge_db"),
+		DBSSLMode:       getEnv("DB_SSLMODE", "disable"),
+		JWTSecret:       getEnv("JWT_SECRET", "secret"),
+		AccessTokenTTL:  15 * time.Minute,
+		RefreshTokenTTL: 7 * 24 * time.Hour,
 	}
 }
 
