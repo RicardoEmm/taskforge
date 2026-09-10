1ab0c480c308ff5a2cee89cb39f6f777abc823c6
Ricardo Emmanuel <remm@Ricardos-MacBook-Pro.local>
2026-09-10 12:05:05 -0600

    feat: add authHandler to router setup and register auth routes as public and protect the others handlers

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,23 +2,32 @@ package router
 
 import (
 	"github.com/RicardoEmm/taskforge/internal/handler"
+	"github.com/RicardoEmm/taskforge/internal/middleware"
+	"github.com/RicardoEmm/taskforge/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
 type Handlers struct {
-	ProductHandler *handler.ProjectHandler
+	AuthHandler    *handler.AuthHandler
+	ProjectHandler *handler.ProjectHandler
 	TaskHandler    *handler.TaskHandler
 	UserHandler    *handler.UserHandler
 }
 
-func Setup(h Handlers) *gin.Engine {
+func Setup(h Handlers, tokenService *service.TokenService) *gin.Engine {
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
 	{
-		registerUserRoutes(api, h.UserHandler)
-		registerTaskRoutes(api, h.TaskHandler)
-		registerProductRoutes(api, h.ProductHandler)
+		registerAuthRoutes(api, h.AuthHandler)
+
+		protected := api.Group("/")
+		protected.Use(middleware.AuthRequired(tokenService))
+		{
+			registerUserRoutes(api, h.UserHandler)
+			registerTaskRoutes(api, h.TaskHandler)
+			registerProductRoutes(api, h.ProjectHandler)
+		}
 	}
 
 	return router
